c62e297a09ed318bf31edfc485fdfd365bfc91e1
Alex Fu <alex.fu914@gmail.com>
2017-11-14 17:11:03 -0500

    Implement simple count down and redraw.

--- app/src/main/java/com/alexfu/countdownview/sample/MainActivity.java
+++ app/src/main/java/com/alexfu/countdownview/sample/MainActivity.java
@@ -1,13 +1,19 @@
 package com.alexfu.countdownview.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.alexfu.countdownview.CountDownView;
 
 public class MainActivity extends AppCompatActivity {
+    private CountDownView cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cv = findViewById(R.id.view_count_down);
+        cv.start();
     }
 }
--- library/src/main/java/com/alexfu/countdownview/CountDownView.java
+++ library/src/main/java/com/alexfu/countdownview/CountDownView.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.text.Layout;
 import android.text.StaticLayout;
@@ -14,9 +15,13 @@ import android.util.TypedValue;
 import android.view.View;
 
 public class CountDownView extends View {
+    private static final int HOUR = 3600000;
+    private static final int MIN = 60000;
+    private static final int SEC = 1000;
     private TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-    private StaticLayout textLayout;
-    private String text = "Hello World";
+    private Layout textLayout;
+    private String text;
+    private CountDownTimer timer;
 
     public CountDownView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -26,12 +31,30 @@ public class CountDownView extends View {
     private void init() {
         textPaint.setColor(Color.BLACK);
         textPaint.setTextSize(dpToPx(20, getResources()));
+        setStartDuration(5000000);
+    }
+
+    public void setStartDuration(long duration) {
+        timer = new CountDownTimer(duration, 1000) {
+            @Override public void onTick(long millis) {
+                updateText(millis);
+                invalidate();
+            }
+
+            @Override public void onFinish() {
+                // No op
+            }
+        };
+        updateText(duration);
+    }
+
+    public void start() {
+        timer.start();
     }
 
     @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        if (textLayout == null || !textLayout.getText().equals(text)) {
-            int textWidth = (int) textPaint.measureText(text);
-            textLayout = new StaticLayout(text, textPaint, textWidth, Layout.Alignment.ALIGN_NORMAL, 0, 0, false);
+        if (textLayout == null) {
+            textLayout = createTextLayout(text);
         }
         setMeasuredDimension(textLayout.getWidth(), textLayout.getHeight());
     }
@@ -39,6 +62,23 @@ public class CountDownView extends View {
     @Override protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         textLayout.draw(canvas);
+    }
+
+    void updateText(long duration) {
+        text = generateCountdownText(duration);
+        textLayout = createTextLayout(text);
+    }
+
+    Layout createTextLayout(String text) {
+        int textWidth = (int) textPaint.measureText(text);
+        return new StaticLayout(text, textPaint, textWidth, Layout.Alignment.ALIGN_NORMAL, 0, 0, false);
+    }
+
+    static String generateCountdownText(long duration) {
+        int hr = (int) (duration / HOUR);
+        int min = (int) ((duration - (hr * HOUR)) / MIN);
+        int sec = (int) ((duration - (hr * HOUR) - (min * MIN)) / SEC);
+        return hr + "h " + min + "m " + sec + "s ";
     }
 
     private static float dpToPx(int dp, Resources resources) {
